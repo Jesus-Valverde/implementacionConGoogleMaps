a547a3b964d9fa3d96217724db6d5dd7ce4fdb50
Jesus Valverde <jesusvalverde.dev@outlook.com>
2025-06-04 22:44:09 -0700

    Subida del proyecto

--- googleMaps/src/main.jsx
+++ googleMaps/src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import '../style.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+import Layout from "./pages/Layout.jsx";
+import Inicio from './pages/inicio.jsx';
+import MapaBasico from './pages/MapaBasico.jsx';
+import RutaDosPuntos from './pages/RutaDosPuntos.jsx';
+import MapaClusters from './pages/MapaClusters.jsx';
+import HerramientasDibujo from './pages/HerramientasDibujo.jsx';
+import RutasDinamicas from './pages/RutasDinamicas.jsx';
+
+const root = createRoot(document.getElementById('root'));
+root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Inicio />} /> 
+          <Route path="MapaBasico" element={<MapaBasico />} />
+          <Route path="RutaDosPuntos" element={<RutaDosPuntos />} />
+          <Route path="MapaClusters" element={<MapaClusters />} />
+          <Route path="HerramientasDibujo" element={<HerramientasDibujo />} />
+          <Route path="RutasDinamicas" element={<RutasDinamicas />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
